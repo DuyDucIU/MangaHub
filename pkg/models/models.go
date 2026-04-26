0c7e10ffcee73b488638cfc2a80c41b806f3e9c5
DuyDucIU <bdduc682@gmail.com>
2026-04-26 15:30:03 +0700

    feat: add shared data models (User, Manga, UserProgress)

---
+++ pkg/models/models.go
@@ -0,0 +1,29 @@
+package models
+
+import "time"
+
+type User struct {
+	ID           string    `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type Manga struct {
+	ID            string   `json:"id"`
+	Title         string   `json:"title"`
+	Author        string   `json:"author"`
+	Genres        []string `json:"genres"`
+	Status        string   `json:"status"`
+	TotalChapters int      `json:"total_chapters"`
+	Description   string   `json:"description"`
+}
+
+type UserProgress struct {
+	UserID         string    `json:"user_id"`
+	MangaID        string    `json:"manga_id"`
+	CurrentChapter int       `json:"current_chapter"`
+	Status         string    `json:"status"`
+	UpdatedAt      time.Time `json:"updated_at"`
+}
